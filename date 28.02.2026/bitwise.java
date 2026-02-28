6b8eb51994f0597f2da20517834ad133c38408a5
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-02-28 07:21:38 +0530

    Add files via upload

---
+++ date 28.02.2026/bitwise.java
@@ -0,0 +1,11 @@
+public class bitwise{
+    public static void main(String[] args){
+        int n=59;
+        if((n|1)>n){
+            System.out.println("Number is Even");
+        }
+        else {
+            System.out.println("Number is odd");
+        }
+    }
+}
---
+++ date 28.02.2026/bitwise_And.java
@@ -0,0 +1,11 @@
+public class bitwise_And{
+    public static void main(String[] args){
+        int n=91;
+        if((n&1)==1){
+            System.out.println("Number is odd");
+        }
+        else{
+            System.out.println("Number id even");
+        }
+    }
+}
