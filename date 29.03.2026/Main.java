f305834648aff8ad78e0d362f0a35fcdb0a99cf8
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-03-29 22:22:10 +0530

    Add files via upload

---
+++ date 29.03.2026/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            int profit = prices[i] - minPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }  }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of days: ");
+        int n = sc.nextInt();
+
+        int[] prices = new int[n];
+        System.out.println("Enter stock prices:");
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+        int result = maxProfit(prices);
+        System.out.println("Maximum Profit: " + result);
+        sc.close();
+    }
+}
---
+++ date 29.03.2026/PrimeRange.java
@@ -0,0 +1,20 @@
+class PrimeRange {
+    public static void main(String[] args) {
+        int n = 50;
+
+        for (int num = 2; num <= n; num++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
