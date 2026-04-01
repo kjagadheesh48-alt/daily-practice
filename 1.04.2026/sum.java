f6e3ee7a6452b533f7fc34c07892ebf5ee77ee1d
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-04-01 23:14:00 +0530

    Add files via upload

---
+++ 1.04.2026/sum.java
@@ -0,0 +1,10 @@
+public class sum{
+    public static void main(String[] args) {
+        int i = 1, sum = 0;
+        while (i <= 10) {
+            sum += i;
+            i++;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
