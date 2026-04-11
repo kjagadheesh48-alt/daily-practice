207991ffa83884bcef533e437cd3d0be545bcab8
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-04-11 23:17:05 +0530

    Add files via upload

---
+++ 10.04.2026/arm.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class arm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int original = num;
+        int sum = 0;
+
+        for (; num != 0; num /= 10) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+        }
+
+        if (sum == original)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not Armstrong Number");
+    }
+}
