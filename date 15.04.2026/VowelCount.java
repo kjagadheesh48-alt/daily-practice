9ef3d4fba250d2adc14c87df64322e90c251a753
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-04-15 20:32:16 +0530

    Add files via upload

---
+++ date 15.04.2026/VowelCount.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class VowelCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = Character.toLowerCase(str.charAt(i));
+            if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+                count++;
+            }
+        }
+        System.out.println("Vowels: " + count);
+    }
+}
