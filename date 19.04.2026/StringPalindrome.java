4df00406e33f5ab8f7de4be754cd81f85a606d4c
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-04-19 21:15:38 +0530

    Add files via upload

---
+++ date 19.04.2026/StringPalindrome.java
@@ -0,0 +1,18 @@
+public class StringPalindrome {
+    public static void main(String[] args) {
+        String s = "madam";
+        boolean flag = true;
+
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i)!=s.charAt(s.length()-1-i)){
+                flag = false;
+                break;
+            }
+        }
+
+        if(flag)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
