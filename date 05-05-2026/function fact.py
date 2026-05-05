41b65bfdc6cc8043474306bc21a4af08f369840b
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-05-05 19:41:28 +0530

    Add files via upload

---
+++ date 05-05-2026/function fact.py
@@ -0,0 +1,8 @@
+def calculate_factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+number = 5
+print(f"The factorial of {number} is {calculate_factorial(number)}")
