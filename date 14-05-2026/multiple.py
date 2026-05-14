0825783ad37e3773c96bb6e9d1957d2eb20a74be
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-05-14 19:46:07 +0530

    Add files via upload

---
+++ date 14-05-2026/multiple.py
@@ -0,0 +1,3 @@
+n=int(input("Enter mulpication table number:"))
+for i in range(1,11):
+    print(n,"*",i,"=",n*i)
---
+++ date 14-05-2026/voter.py
@@ -0,0 +1,5 @@
+n=int(input("Enter your age: "))
+if n>=18:
+    print("eligible to vote")
+else:
+    print("not eligible to vote")
