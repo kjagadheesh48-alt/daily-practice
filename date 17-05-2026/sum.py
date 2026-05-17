4710d3e6c4787788dbfcbf43e72c4ffc220833a8
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-05-17 19:17:53 +0530

    Add files via upload

---
+++ date 17-05-2026/sum.py
@@ -0,0 +1,11 @@
+def sum_digits(n):
+    total = 0
+
+    while n > 0:
+        total += n % 10
+        n = n // 10
+
+    return total
+
+num = int(input("Enter a number: "))
+print("Sum of digits =", sum_digits(num))
