8610c4b9b109d24aadc496b08fd087958e24a400
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-05-18 19:38:38 +0530

    Add files via upload

---
+++ date 18-05-2026/ava.py
@@ -0,0 +1,8 @@
+def average(a, b, c):
+    return (a + b + c) / 3
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+z = int(input("Enter third number: "))
+
+print("Average =", average(x, y, z))
---
+++ date 18-05-2026/count.py
@@ -0,0 +1,6 @@
+def count_characters(text):
+    return len(text)
+
+word = input("Enter text: ")
+
+print("Number of characters =", count_characters(word))
