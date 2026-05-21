626d6498f644b5b4f7c2b3a45e4d53e6558199d1
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-05-21 07:17:04 +0530

    Add files via upload

---
+++ mulp.py
@@ -0,0 +1,7 @@
+def multiply(a, b):
+    return a * b
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Product =", multiply(x, y))
---
+++ str.py
@@ -0,0 +1,5 @@
+def string_length(text):
+    return len(text)
+
+word = input("Enter text: ")
+print("Length =", string_length(word))
