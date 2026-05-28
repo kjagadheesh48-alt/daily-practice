803221f7fbe864a8fac4ef553dd7ed28d5b8ba3a
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-05-28 21:17:41 +0530

    Add files via upload

---
+++ date 28-05-2026/sum.py
@@ -0,0 +1,8 @@
+def digit_sum(n):
+    s = 0
+    while n > 0:
+        s += n % 10
+        n //= 10
+    return s
+
+print(digit_sum(123))
---
+++ date 28-05-2026/swap.py
@@ -0,0 +1,5 @@
+def swap(a, b):
+    return b, a
+
+x, y = swap(5, 10)
+print(x, y)
