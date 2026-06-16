642a13067a630264736a08db5eadb407476e7f70
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-06-16 18:58:11 +0530

    Add files via upload

--- fact.java
+++ fact.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
-public class fact{
+public class fact {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
         int n = sc.nextInt();
 
         int i = 1;
         int fact = 1;
 
-        while (i <= n) {
-            fact *= i;
+        while(i <= n) {
+            fact = fact * i;
             i++;
         }
 
