07e57c62e94c45ea92033dcdfc6f2bf15bcafac3
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-06-22 21:20:24 +0530

    Add files via upload

---
+++ date 22-06-2026/java.java
@@ -0,0 +1,12 @@
+class java{
+    public static void main(String[] args){
+            char a='(';
+            System.out.println((int) a);
+            char b='Q';
+            System.out.println((int) b);
+            char c='!';
+            System.out.println((int) c);
+            char d='O';
+            System.out.println((int) d);
+    }
+}
