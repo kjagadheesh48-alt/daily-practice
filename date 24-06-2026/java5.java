3853258da18cd7bd1b0b04d66119fc3b06ee37ab
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-06-24 21:36:31 +0530

    Add files via upload

---
+++ date 24-06-2026/java5.java
@@ -0,0 +1,6 @@
+public class java5{
+    public static void main(String[] args) {
+        int a = -8;
+        int b = 12;
+        System.out.println((5<<2));
+    }}
---
+++ date 24-06-2026/java6.java
@@ -0,0 +1,6 @@
+public class java3{
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 12;
+        System.out.println((a & b));
+    }}
