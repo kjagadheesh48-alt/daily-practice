daf453e2d9b349007315bb671b74f2ea8d34b1ca
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-06-25 21:38:28 +0530

    Add files via upload

---
+++ date 25-06-2026/java12.java
@@ -0,0 +1,8 @@
+class java12{
+    public static void main(String[] args){
+        int a[]={1,2,3,4,5};
+        for(int b=0;b<a.length-1;b++){
+            System.out.println(a[b]);
+        }
+    }
+}
---
+++ date 25-06-2026/java7.java
@@ -0,0 +1,6 @@
+public class java7{
+    public static void main(String[] args) {
+        int a = -8;
+        int b = 12;
+        System.out.println((18>>2));
+    }}
