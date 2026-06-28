0dc6faa561cc1915051a53e93d98dd7a3bcde0bf
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-06-28 21:23:25 +0530

    Add files via upload

---
+++ date 28-06-2026/java39.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class java39{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a salary: ");
+        int salary = sc.nextInt();
+        if salary>=50000;
+        
+        
---
+++ date 28-06-2026/java43.java
@@ -0,0 +1,10 @@
+public class java43 {
+    public static void main(String[] args){
+        for(int a=5;a>=0;a--){
+            System.out.print("*");
+            for(int b=1;b<=a;b++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }}
