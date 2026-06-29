51578b47902fc016a671ad0cf643781d563e8027
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-06-29 20:27:48 +0530

    Add files via upload

---
+++ date 29-06-2026/java44.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class java44{
+    public static void main(String[] args){
+        int a[]=new int[5];
+        a[0]=121;
+        a[3]=20;
+        a[4]=5;
+        System.out.print(Arrays.toString(a));
+
+    }
+}
---
+++ date 29-06-2026/java55.java
@@ -0,0 +1,11 @@
+public class java55 {
+    public static void main(String[] args){
+        int a=10;
+        do{
+            System.err.println("Hello, World!");
+            a--;
+            
+        }
+        while(a>1);
+    }
+}
