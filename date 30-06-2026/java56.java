6270f79c5a3cbeb3baf32da9555f82754f2936c6
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-06-30 21:03:36 +0530

    Add files via upload

---
+++ date 30-06-2026/java56.java
@@ -0,0 +1,10 @@
+
+import java.util.Arrays;
+public class java56{
+    public static void main(String[] args){
+        int a[]={1,56,85,9,656,};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+
+    }
+}
---
+++ date 30-06-2026/java65.java
@@ -0,0 +1,8 @@
+public class java65{
+    public static void main(String[] args){
+        int a[]={1,56,85,9,656,};
+        Arrays.sort(a,Collections.reverseOrder());
+        System.out.println(Arrays.toString(a));
+
+    }
+}
