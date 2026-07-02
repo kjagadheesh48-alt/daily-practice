965360fdcc9eff7453a99756da815692fc082afb
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-07-02 22:46:45 +0530

    Add files via upload

---
+++ date 2-07-2026/java76.java
@@ -0,0 +1,15 @@
+public class java76{
+    public static void main(String[] args) {
+        int a[] = {1, 56, 85, 9, 656};
+
+        int m= a[0];
+
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < m) {
+                m = a[i];
+            }
+        }
+
+        System.out.println("Minimum value = " + m);
+    }
+}
