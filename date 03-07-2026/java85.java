2d390349619842a0d11e0089aa25d2ff9001d5ca
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-07-03 23:14:19 +0530

    Add files via upload

---
+++ date 03-07-2026/java85.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+public class java85{
+    public static void main(String[] args) {
+        int a[] = {1, 56, 85, 9, 656};
+        Arrays.sort(a);
+        System.out.println(a[a.length-1]);
+    }}
