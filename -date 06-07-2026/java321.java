4838fc9cd9bde1a6a9e38ff58855dd2e5523e28a
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-07-06 23:03:17 +0530

    Add files via upload

---
+++ -date 06-07-2026/java321.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class java321{
+    public static void main(String[] args){
+        int a[]={1,2,3,4,5};
+        int b[]={1,2,3,4,5};
+        System.out.println(Arrays.binarySearch(a,4));
+        System.out.println(Arrays.equals(a,b));
+    }
+}
