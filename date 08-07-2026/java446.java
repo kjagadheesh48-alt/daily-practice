104e275448175c078fc5f176072ecde5bd4e29db
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-07-08 21:41:58 +0530

    Add files via upload

---
+++ date 08-07-2026/java446.java
@@ -0,0 +1,19 @@
+
+public class java446{
+    public static void main(String[] args){
+        int a=15;
+        if((a%5==0)&&(a%3==0)){
+            System.out.print("Fizz Buzz");
+        }
+        else if(a%3==0){
+            System.out.print("Buzz");
+        }
+        else if(a%5==0){
+            System.out.print("Fizz");
+
+        }
+        else{
+            System.out.print("invalid");
+        }
+    }}
+    
