6dea9227b7a3104329b018d49f92d849a46cd688
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-07-10 20:38:58 +0530

    Add files via upload

---
+++ date 10-07-2026/java741.java
@@ -0,0 +1,12 @@
+
+import java.util.Arrays;
+class java741{
+    public static void main(String[] args){
+        String a[]=new String[6];
+        a[1]="3rd";
+        a[3]="1st";
+        a[4]="2nd";
+
+        System.out.print(Arrays.toString(a));
+    }
+}
