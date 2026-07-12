6106c177b1e688042abfdbef413efb3551b55965
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-07-12 23:33:12 +0530

    Add files via upload

---
+++ date 12-07-2026/class java12.java
@@ -0,0 +1,12 @@
+class java12 {
+    public static void main(string [] bala){
+        int a [][] = new int [3][3];
+        a[0][2] = 90;
+        a[1][2] = 20;
+        a[2][1] = 10;
+        a[1][1] = 60;
+        {
+        System.out.println(Arrays.deeptoString(a));
+        }
+    }
+}
