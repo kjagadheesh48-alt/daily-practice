a9c81e2e1cbcba93ac453381f6667acb48a859cb
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-07-20 23:49:16 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,13 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String original = "Java";
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
+}
