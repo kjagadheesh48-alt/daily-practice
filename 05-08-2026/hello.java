98d8f6611aefe2d7b9fd4c133a48cc095d377f20
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-08-05 22:52:44 +0530

    Add files via upload

---
+++ 05-08-2026/hello.java
@@ -0,0 +1,8 @@
+class hello{
+    public static void main(String args[]){
+        int a=56;
+
+        System.err.println(a);
+
+    }
+    }
---
+++ 05-08-2026/hello1.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class hello1 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.err.println("Enter the number that have to be readed");
+        int a=sc.nextInt();
+        System.out.print("THE READED number is "+a);
+
+    }
+    
+}
