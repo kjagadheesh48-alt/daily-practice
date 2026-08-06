6193786ea6d8da0f6ee9ef072a8e5de71b5b5ea1
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-08-06 20:23:54 +0530

    Add files via upload

---
+++ 06-08-20206/add.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class add{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the first number");
+        int F=sc.nextInt();
+        System.out.println("THE secound number");
+        int s=sc.nextInt();
+        System.out.println("THE SUM OF THE TWO NUMBER"+(F+s));
+
+    }
+}
---
+++ 06-08-20206/sub.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class sub{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("the number");
+        float a=sc.nextFloat();
+         System.out.println("the numbe3");
+        float b=sc.nextFloat();
+        System.out.println("the number is"+(a-b));
+    }
+    }
