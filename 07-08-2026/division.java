5db36cabf4bfb32eb650faa521f781168de8063c
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-08-07 20:12:34 +0530

    Add files via upload

---
+++ 07-08-2026/division.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class division{
+    public static void main(String[] args){
+        Scanner b=new Scanner(System.in);
+        System.out.println("THE NUMBER DIVIDEND");
+        double a= b.nextDouble();
+        System.out.println("THE DIVISOR");
+        double c=b.nextDouble();
+        System.out.println("THE number"+(a/c)); 
+
+    }
+}
---
+++ 07-08-2026/m.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class m{
+    public static void main(String[] args) {
+        Scanner pc=new Scanner(System.in);
+        System.out.println("the first number to be multi");
+        short a=pc.nextShort();
+        System.out.println("THE secound number");
+        short b=pc.nextShort();
+        System.out.println("THE MULTIPULR NUMBER"+a*b);
+
+    }
+}
