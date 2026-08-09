10f5913b4efed0652d2d438bea99c089f285e339
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-08-09 22:08:24 +0530

    Add files via upload

---
+++ 09-08-2026/cube.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class cube{
+    public static void main(String[] args){
+        Scanner bc= new Scanner(System.in);
+        System.out.println("THE CUDE");
+        double a=bc.nextDouble();
+        double b=Math.pow(a,3);
+        System.out.println("the cude of the number"+b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+    }
+}
---
+++ 09-08-2026/square.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class square{
+    public static void main(String[] args) {
+        Scanner pc=new Scanner(System.in);
+        System.out.print("The square number");
+        float a=pc.nextFloat();
+        double b=Math.pow(a,2);
+        System.out.print("The square "+b);
+
+
+    }
+}
