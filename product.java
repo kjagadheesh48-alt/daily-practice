2bb10326a08549fc3fe0b6609fd68ae763ee7f6c
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-08-13 14:20:17 +0530

    Add files via upload

--- product.java
+++ product.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
-public class count{
+public class product{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("THE NUMBER");
         int a=sc.nextInt();
-        int count=0;
+        int pro=1;
         while(a!=0){
+            int d=a%10;
+            pro=pro*d;
             a=a/10;
-            count=count+1;
+
         }
-        System.out.println("THE count OF the NUMBER"+count);
+        System.out.println("THE SUM OF the NUMBER"+pro);
     }
 }
