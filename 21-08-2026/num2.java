7519415a44573d061549c0736428cc69d5ae0f3a
C JAGADHEESH KUMAR <kjagadheesh48@gmail.com>
2026-08-21 21:56:57 +0530

    Add files via upload

---
+++ 21-08-2026/num2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class num2{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("THE NUMBER");
+        int n=sc.nextInt();
+        for(int i=n;i>=0;i--){
+            System.out.println("THE NUMBER  :"+i);
+
+        }
+    }
+}
